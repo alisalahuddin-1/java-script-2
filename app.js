27d920a1375a684938d09767c8cafe73f87d22a0
Ali Salahuddin <alisalahuddin91@gmail.com>
2024-08-23 19:09:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const student = "Ali" {
+    age :12;
+    cgpa : 2.8;
+    pass : yeesss;
+};
---
+++ script.js
@@ -0,0 +1,28 @@
+// chapter 1  java script
+//pdf no 1 alert
+alert ("Error! Please enter a valid Password");
+//qno 3 line break
+alert("Welcome to Js land... " + "\n" + "Happy coding!");
+//qno 4
+alert("Happy coding" + "\n" + "Prevent this page for creating additional dailog..")
+//pdf 2 variable
+var username = "Alisalahuddin"
+var massage = "Hello world"
+console.log(massage);
+alert(massage);
+//qno 2
+alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P")
+//qno 3
+alert(" ")
+// document write
+var massage1 = "Yeah ! ican do an html code"
+document.write (massage1)
+//pdf 3
+var age = "Iam 18 year old"
+age--
+console.log (age)
+//variable illegal or legal
+var legal = "name,username,$,name1, "
+document.write (legal)
+var illegal = "1name , @"
+document.write (illegal)
